8ecf0695a93ba1e24ae93f1b3a3cf8ba1b15b26c
Niraj Bista <admin@karnali.com>
2020-01-30 23:46:08 -0500

    build while git commitgit add .!

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 @app.route('/')
 def hello():
     """Return a friendly HTTP greeting."""
-    return 'Hello World! Project in GCP - flask-gcp-helloflask.  Add promote.!!!'
+    return 'Hello World! Project in GCP build while git commit!!!'
 
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port=8080, debug=True)
