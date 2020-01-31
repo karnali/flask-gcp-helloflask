136c5b21c4a36fb19b3615b5d973fc0929847a32
karnali <48341455+karnali@users.noreply.github.com>
2020-01-30 23:29:48 -0500

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 @app.route('/')
 def hello():
     """Return a friendly HTTP greeting."""
-    return 'Hello World! Project in GCP - flask-gcp-helloflask!!!'
+    return 'Hello World! Project in GCP - flask-gcp-helloflask.  Add promote.!!!'
 
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port=8080, debug=True)
